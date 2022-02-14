7f8f71c106e00400401e6c347099ec4f864fc09e
kolomyaka1 <ka4ok24@mail.ru>
2022-02-14 15:19:38 +0300

    check

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import pizzaLogo from '../../assets/img/pizza-logo.svg';
+import ShoppingButton from "./button/ShoppingButton"
+
+const Header = () => {
+    return (
+    <div className="header">
+        <div className="container">
+            <div className="header__logo">
+                <img width="38" src={pizzaLogo} alt="Pizza logo" />
+                <div>
+                <h1>React Pizza</h1>
+                <p>самая вкусная пицца во вселенной</p>
+                </div>
+            </div>
+            <ShoppingButton />
+        </div>
+    </div>
+    )
+}
+
+export default Header;
