e74816464142ecb148f3626a4fb353b7805a9c49
kolomyaka1 <ka4ok24@mail.ru>
2022-02-15 19:55:47 +0300

    add pizzaBlock and edit size and types for pizzaEl
    also add axios and we get data from json with axios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import { Header } from './components';
 import { Home, Cart } from './pages'
-
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import axios from 'axios'
 
 import './scss/app.scss'
 
 
-
-
 function App() {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    // Отправляем запрос для получения db
+    
+    // 1. Запрос через fetch
+      // fetch('http://localhost:3001/db.json')
+      // .then((res) => res.json())
+      // .then(json => {
+      //   setPizzas(json.pizzas)
+      // })
+
+    // 2. Запрос через Axios
+    axios.get('http://localhost:3001/db.json').then(({data}) => {
+      setPizzas(data.pizzas)
+    });
+  },[]);
 
 
   return (
@@ -16,7 +33,7 @@ function App() {
     <Header />
     <div className="content">
       <Routes >
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home pizzas={pizzas}/>} />
         <Route path='/cart' element={<Cart />} />
       </Routes>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -2,3 +2,4 @@
 export { default as Header } from './Header/Header.tsx';
 export { default as Categories } from './Categories/Categories.jsx';
 export { default as SortPopup } from './SortPopup/SortPopup.jsx';
+export { default as PizzaBlock } from './PizzaBlock/PizzaBlock.jsx';
